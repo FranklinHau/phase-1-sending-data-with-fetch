6e99aa501491d06741a51a7d3c756e52d7a71c5d
Franklin Hau <fhauchi@gmail.com>
2023-04-24 08:45:33 -0600

    sendingDataFetch

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+// const { bodyParser } = require("json-server")
+
 // Add your code here
+
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({ name, email })
+    })
+    .then(res => res.json())
+    .then(data => {console.log(data)
+
+  const newId = data.id;
+  const idElement = document.createElement('p');
+  
+  idElement.textContent = `New ID: ${newId}`;
+  document.body.appendChild(idElement);
+  return data;   
+})
+.catch(error => {
+    console.error("Error submitting data:", error);
+    const errorElement = document.createElement('p');
+    errorElement.textContent = `Error: ${error.message}`;
+    document.body.appendChild(errorElement);
+  });
+}
